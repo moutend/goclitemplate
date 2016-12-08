cfae13bf90e2770d21e2f3eca98fbb8b81fa8c38
Yoshiyuki Koyanagi <moutend@gmail.com>
2016-12-09 05:01:58 +0900

    Add comments

--- goclitemplate.go
+++ goclitemplate.go
@@ -1,16 +1,22 @@
+// Author: Yoshiyuki Koyanagi <moutend@gmail.com>
+// License: mIT
+
+// Package goclitemplate provides useless functions.
 package goclitemplate
 
 import (
 	"fmt"
 )
 
-var revision string = "latest"
-var version string = "latest"
+var revision = "latest"
+var version = "latest"
 
+// Version returns version and revision as string.
 func Version() string {
 	return fmt.Sprintf("%s-%s", version, revision)
 }
 
+// Hello returns a greeting message like "Hello, James!".
 func Hello(name string) string {
 	return fmt.Sprintf("Hello, %s!", name)
 }
